64cfa392acd7c356fd170527c760d58bd230e1eb
Taylor Elam <taylordeanelam@gmail.com>
2022-06-17 12:12:14 -0500

    Initial Commit

---
+++ Tripasaurus/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Tripasaurus
+//
+//  Created by Taylor Elam on 6/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tripasaurus/TripasaurusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripasaurusApp.swift
+//  Tripasaurus
+//
+//  Created by Taylor Elam on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct TripasaurusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
