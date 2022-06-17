45bd65975b00f53d96dfcfe3ee6ab117619828a9
Taylor Elam <taylordeanelam@gmail.com>
2022-06-17 12:46:27 -0500

    Add TabView (#1)
    * Added AppTab enum
    * Added TabView

--- Tripasaurus/TripasaurusApp.swift
+++ Tripasaurus/TripasaurusApp.swift
@@ -2,9 +2,31 @@ import SwiftUI
 
 @main
 struct TripasaurusApp: App {
+    @State private var id = UUID()
+    @State private var tabSelection = 0
+
+    var handler: Binding<Int> { Binding(
+        get: { self.tabSelection },
+        set: {
+            self.id = UUID()
+            self.tabSelection = $0
+        }
+    ) }
+
     var body: some Scene {
         WindowGroup {
-            EventListView()
+            TabView {
+                ForEach(AppTab.allCases) { tab in
+                    NavigationView {
+                        tab.content
+                            .navigationTitle(tab.title)
+                            .id(id)
+                    }
+                    .navigationViewStyle(.stack)
+                    .tabItem { Label(tab.title, systemImage: tab.icon) }
+                    .tag(tab.tag)
+                }
+            }
         }
     }
 }
