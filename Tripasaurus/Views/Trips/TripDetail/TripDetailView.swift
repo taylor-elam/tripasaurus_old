9bd5af4f8a8e83d0ccdb8ff542016c53d42591b0
Taylor Elam <taylordeanelam@gmail.com>
2022-06-18 08:14:59 -0500

    Added Delete button to TripDetail

--- Tripasaurus/Views/Trips/TripDetail/TripDetailView.swift
+++ Tripasaurus/Views/Trips/TripDetail/TripDetailView.swift
@@ -9,40 +9,53 @@ struct TripDetailView: View {
 
     var body: some View {
         VStack {
-            VStack {
-                TextField("Title", text: $tripCopy.title)
-                    .multilineTextAlignment(.center)
-                    .font(.title2)
-                Divider().frame(height: 1)
-                DateSection(
-                    date: $tripCopy.startDate,
-                    displayComponents: [.date],
-                    id: "startDate",
-                    isSelected: selection == "startDate",
-                    label: "Start Date",
-                    selectRow: selectDeselect,
-                    timeFormat: .omitted
-                )
-                Divider().frame(height: 1)
-                DateSection(
-                    date: $tripCopy.endDate,
-                    displayComponents: [.date],
-                    id: "endDate",
-                    isSelected: selection == "endDate",
-                    label: "End Date",
-                    selectRow: selectDeselect,
-                    timeFormat: .omitted
-                )
+            ScrollView {
+                VStack {
+                    TextField("Title", text: $tripCopy.title)
+                        .multilineTextAlignment(.center)
+                        .font(.title2)
+                    Divider().frame(height: 1)
+                    DateSection(
+                        date: $tripCopy.startDate,
+                        displayComponents: [.date],
+                        id: "startDate",
+                        isSelected: selection == "startDate",
+                        label: "Start Date",
+                        selectRow: selectDeselect,
+                        timeFormat: .omitted
+                    )
+                    Divider().frame(height: 1)
+                    DateSection(
+                        date: $tripCopy.endDate,
+                        displayComponents: [.date],
+                        id: "endDate",
+                        isSelected: selection == "endDate",
+                        label: "End Date",
+                        selectRow: selectDeselect,
+                        timeFormat: .omitted
+                    )
+                }
+                .padding()
+                .listCardStyle()
+                .padding(.horizontal)
             }
-            .padding()
-            .listCardStyle()
-            .padding(.horizontal)
-
-            Spacer()
+            
+            Button(role: .destructive, action: {
+                trip.isDeleted = true
+                dismiss()
+            }, label: {
+                Text("Delete Trip").foregroundColor(Color(UIColor.systemRed))
+            })
+            .frame(maxWidth: .infinity, alignment: .center)
+            .padding(.vertical)
+            .background(.white.opacity(0.7))
+            .overlay(Divider(), alignment: .top)
+            .overlay(Divider(), alignment: .bottom)
         }
         .onAppear { tripCopy = trip }
         .background(Color(UIColor.secondarySystemBackground))
         .navigationBarTitle("", displayMode: .inline)
+        // TODO: add styling to navbar
         .toolbar {
             ToolbarItem(placement: .confirmationAction) {
                 Button("Save") { saveTrip() }.disabled(tripCopy.title.isEmpty)
