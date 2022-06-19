8c2041b9687a2712161c0a91f61c575abc7e5f8a
Taylor Elam <taylordeanelam@gmail.com>
2022-06-18 23:48:11 -0500

    Added more details to FlightDetail

--- Tripasaurus/Views/Trips/TripDetail/Flights/FlightDetail.swift
+++ Tripasaurus/Views/Trips/TripDetail/Flights/FlightDetail.swift
@@ -9,13 +9,53 @@ struct FlightDetail: View {
         return formatter
     }
 
+    var numberFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+
     var body: some View {
         VStack {
-            FlightMainDetails(reservation: $reservation, dateFormatter: dateFormatter)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding()
-                .listCardStyle()
-                .padding(.horizontal)
+            VStack(alignment: .leading, spacing: 10) {
+                FlightMainDetails(reservation: $reservation, dateFormatter: dateFormatter)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+                if !reservation.confirmationNumber.isEmpty || reservation.cost > 0 || !reservation.notes.isEmpty {
+                    Divider()
+                }
+
+                HStack {
+                    if !reservation.confirmationNumber.isEmpty {
+                        VStack(alignment: .leading) {
+                            Text("Confirmation #").font(.caption).foregroundColor(.secondary)
+                            Text(reservation.confirmationNumber)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+
+                    if reservation.cost > 0 {
+                        VStack(alignment: .leading) {
+                            Text("Cost").font(.caption).foregroundColor(.secondary)
+                            Text(numberFormatter.string(from: reservation.cost as NSNumber) ?? "")
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
+
+                if !reservation.notes.isEmpty {
+                    VStack(alignment: .leading) {
+                        Text("Notes").font(.caption).foregroundColor(.secondary)
+                        Text(reservation.notes)
+                        // TODO: Add More/Less button and truncate notes
+                    }
+                }
+            }
+            .padding()
+            .listCardStyle()
+            .padding(.horizontal)
+
             Spacer()
         }
         .background(Color(UIColor.secondarySystemBackground))
